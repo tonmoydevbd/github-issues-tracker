b1499bbb3491d9877874ac5a90485edf9e5e1194
tonmoydevbd <tonmoydevbd@gmail.com>
2026-03-07 14:19:20 +0600

    added sign in functionality.

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+const nameEl = document.querySelector('#name');
+const passwordEl = document.querySelector('#password');
+const signInBtn = document.querySelector('#sign-in-btn');
+const errorMessage = document.querySelector('.error-message');
+
+
+signInBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const isValidName = nameEl.value === 'admin';
+  const isValidPassword = passwordEl.value === 'admin123';
+  if (isValidName && isValidPassword) {
+    window.location.assign('./dashbord.html');
+  } else {
+    errorMessage.classList.remove('hide');
+    nameEl.value = '';
+    passwordEl.value = '';
+    return;
+  }
+})
